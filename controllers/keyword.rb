f45912cf0563f42490f650b73623193008a4af62
yiweihuang <n1993212@gmail.com>
2016-11-16 14:54:45 +0800

    delete show keyword app

--- controllers/keyword.rb
+++ controllers/keyword.rb
@@ -51,32 +51,7 @@ class KeywordCloudApp < Sinatra::Base
       slim(:home)
     end
   end
-
-  get '/keyword/:uid/:course_id/chapter/:chapter_id/showkeyword' do
-    if @current_uid && @current_uid.to_s == params[:uid]
-      @auth_token = session[:auth_token]
-      @course_id = params[:course_id]
-      @chapter_id = params[:chapter_id]
-      keyword_url = "/keyword/#{@current_uid}/#{@course_id}/chapter"
-      @folder = GetOwnedFolder.call(current_uid: @current_uid,
-                                    auth_token: session[:auth_token],
-                                    course_id: @course_id,
-                                    folder_type: 'slides')
-      @keyword = ShowKeyword.call(current_uid: @current_uid,
-                                  auth_token: @auth_token,
-                                  course_id: @course_id,
-                                  chapter_id: @chapter_id)
-      @keyword_chid = HasKeywordChap.call(current_uid: @current_uid,
-                                          auth_token: session[:auth_token],
-                                          course_id: @course_id)
-      @ordered_folder = @folder.sort_by{ |chapter| chapter[:chapter_order] }
-      # slim(:show_keywords)
-      redirect keyword_url
-    else
-      slim(:home)
-    end
-  end
-
+  
   post '/keyword/:uid/:course_id/chapter/:chapter_id/postkeyword/' do
     if @current_uid && @current_uid.to_s == params[:uid]
       @auth_token = session[:auth_token]
--- services/show_keyword.rb
+++
@@ -1,9 +0,0 @@
-require 'http'
-
-class ShowKeyword
-  def self.call(current_uid:, auth_token:, course_id:, chapter_id:)
-    response = HTTP.auth("Bearer #{auth_token}")
-                   .get("#{ENV['API_HOST']}/accounts/#{current_uid}/#{course_id}/#{chapter_id}/showkeyword")
-    response.code == 200 ? response.parse : []
-  end
-end
